30595496a7735d9b74900a6acb5fcb17a626ab99
Void Zone <developer+github@voidzone.pl>
2016-03-10 00:33:53 +0100

    First structure preparations

---
+++ src/AdminBundle/Controller/AlbumController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace AdminBundle\Controller;
+
+
+use Symfony\Component\HttpFoundation\Response;
+
+class AlbumController {
+
+    public function indexAction()
+    {
+        return new Response('');
+    }
+} 
---
+++ src/VoidZoneBundle/VoidZoneBundle.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace VoidZoneBundle;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class VoidZoneBundle extends Bundle
+{
+}
